8c3b04c152caf929f47227a29828630a885e6bfc
Macroverse <macroverse@protonmail.com>
2017-06-15 00:05:11 -0700

    Learn how to web3 good and test the crowdsale limit

--- contracts/MRVToken.sol
+++ contracts/MRVToken.sol
@@ -2,26 +2,97 @@ pragma solidity ^0.4.11;
 
 
 import "./zeppelin/token/StandardToken.sol";
+import "./zeppelin/ownership/Ownable.sol";
 
 
 /**
  * MRV token, distributed by crowdsale.
  * Derived from OpenZeppelin CrowdsaleToken template.
  */
-contract MRVToken is StandardToken {
+contract MRVToken is StandardToken, Ownable {
 
     string public constant name = "Macroverse Token";
     string public constant symbol = "MRV";
-    uint public constant decimals = 18;
+    uint public decimals;
     address public multisig;
+    
+    // How many MRV can be sold in the crowdsale?
+    uint public maxSupply;
+    
+    bool crowdsaleStarted;
+    bool crowdsaleEnded;
 
     /**
     * Deploy a new MRVToken contract, paying to the given multisig wallet.
     */
     function MRVToken(address _multisig) {
         multisig = _multisig;
+        decimals = 18; // Start with 18 decimals, same as ETH
+        crowdsaleStarted = false;
+        crowdsaleEnded = false;
+        // Default to a max supply of 100 million tokens available.
+        maxSupply = 100000000;
     }
-
+    
+    /**
+     * Only allow some actions after the crowdsale is over.
+     */
+    modifier onlyAfterClosed {
+        if (!crowdsaleEnded) throw;
+        _;
+    }
+    
+    /**
+     * Only allow some actions before the crowdsale starts.
+     */
+    modifier onlyBeforeOpened {
+        if (crowdsaleStarted) throw;
+        _;
+    }
+    
+    /**
+     * Only allow some actions while the crowdsale is active.
+     */
+    modifier onlyDuringCrowdsale {
+        if (crowdsaleEnded) throw;
+        if (!crowdsaleStarted) throw;
+        _;
+    }
+    
+    /**
+     * Determine if the crowdsale is currently happening.
+     */
+    function isCrowdsaleActive() returns (bool) {
+        return (crowdsaleStarted && !crowdsaleEnded);
+    }
+    
+    /**
+     * Start the crowdsale.
+     */
+    function startCrowdsale() onlyOwner onlyBeforeOpened {
+        crowdsaleStarted = true;
+    }
+    
+    /**
+     * End the crowdsale.
+     */
+    function endCrowdsale() onlyOwner onlyDuringCrowdsale {
+        crowdsaleEnded = true;
+    }
+    
+    /**
+     * When the crowdsale is finished, the contract owner can adjust the decimal places for display purposes.
+     */
+    function setDecimals(uint new_decimals) onlyOwner onlyAfterClosed {
+        decimals = new_decimals;
+    }
+    
+    /**
+     * Before the crowdsale opens, the max token count can be configured.
+     */
+    function setMaxSupply(uint new_max) onlyOwner onlyBeforeOpened {
+        maxSupply = new_max;
+    }
 
     // 1 ether = 5000 MRV
     uint public constant PRICE = 5000;
@@ -38,13 +109,22 @@ contract MRVToken is StandardToken {
     * @dev Creates tokens and send to the specified address.
     * @param recipient The address which will recieve the new tokens.
     */
-    function createTokens(address recipient) payable {
+    function createTokens(address recipient) payable onlyDuringCrowdsale {
         if (msg.value == 0) {
             throw;
         }
 
-        uint tokens = msg.value.mul(getPrice());
-        totalSupply = totalSupply.add(tokens);
+        uint tokens = msg.value.mul(getPrice()); // Exploits the fact that we have 18 decimals, like ETH.
+        
+        var newTotalSupply = totalSupply.add(tokens);
+        
+        if (newTotalSupply > maxSupply * 10 ** 18) {
+            // This would be too many tokens issued.
+            // Don't mess around with partial order fills.
+            throw;
+        }
+        
+        totalSupply = newTotalSupply;
 
         balances[recipient] = balances[recipient].add(tokens);
 
--- test/TestMRVToken.js
+++ test/TestMRVToken.js
@@ -1,16 +1,84 @@
 var MRVToken = artifacts.require("MRVToken");
 
 contract('MRVToken', function(accounts) {
-  it("should pay 5000 MRV per ETH", async function() {
+  it("should start inactive", async function() {
     let instance = await MRVToken.deployed();
     
-    var account_one = accounts[0];
+    let account_one = accounts[0]
+    
+    assert.equal((await instance.isCrowdsaleActive.call()), false, "the crowdsale is initially not started")
+  })
+  
+  it("should allow the crowdsale to start", async function() { 
+    // Start crowdsale
+    // Note that we can't await the isCrowdsaleActive result for some reason; we have to use then.
+    // This may be a bug in web3 or Truffle
+    let instance = await MRVToken.deployed()
+    
+    // DON'T use .call(). .call() confusingly runs things locally, while just () actually sends them.
+    await instance.startCrowdsale()
+
+    assert.equal(await instance.isCrowdsaleActive.call(), true, "the crowdsale starts")
+
+  })
+})
+
+// New test case, for new contract
+contract('MRVToken', function(accounts) {
+  
+  it("should still start inactive", async function() {
+    let instance = await MRVToken.deployed();
+    
+    assert.equal((await instance.isCrowdsaleActive.call()), false, "the crowdsale is initially not started")
+  })
+ 
+  
+  it("should pay 5000 MRV per ETH", async function() {
+    
+    let instance = await MRVToken.deployed()
+    let account_one = accounts[0]
+    
+    await instance.startCrowdsale()
+
+    assert.equal(await instance.isCrowdsaleActive.call(), true, "the crowdsale starts")
+    
     
     // Buy tokens
     await instance.sendTransaction({from: account_one, value: web3.toWei(1, "ether")})
     
     // See if we got them
     assert.equal((await instance.balanceOf.call(account_one)).toNumber(), web3.toWei(5000, "ether"), "the correct number of tokens are issued")
+  })
+})
+
+contract('MRVToken', function(accounts) {
+  it("should allow buying the max number of tokens, but no more", async function() {
+    let instance = await MRVToken.deployed();
+    let account_one = accounts[0]
     
-  });
-});
+    // We need to lower the max because testrpc only grants us so much ETH
+    let MAX_TOKENS = 5000
+    
+    // Set max. Note that this is denominated in WHOLE tokens, not in wei-size units.
+    await instance.setMaxSupply(MAX_TOKENS)
+    
+    // Start crowdsale
+    await instance.startCrowdsale()
+      
+    // Buy tokens
+    await instance.sendTransaction({from: account_one, value: web3.toWei(MAX_TOKENS/5000, "ether")})
+    
+    // See if we got them
+    assert.equal((await instance.balanceOf.call(account_one)).toNumber(), web3.toWei(MAX_TOKENS, "ether"), "all tokens can be issued")
+    
+    // Fail to get any more
+    await instance.sendTransaction({from: account_one, value: web3.toWei(2/5000, "ether")}).then(function () {
+      assert.ok(false, "successfully bought excess tokens")
+    }).catch(async function () {
+      assert.equal((await instance.balanceOf.call(account_one)).toNumber(), web3.toWei(MAX_TOKENS, "ether"), "no more tokens than the max can be issued")
+    })
+    
+    
+  })
+  
+})
