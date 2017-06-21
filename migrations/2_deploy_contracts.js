7686eac475da0836a8ec843830f8dbe9d5fe8dd8
Macroverse <macroverse@protonmail.com>
2017-06-21 16:19:24 -0700

    Drop the SingleStarPrototype and deploy the registry

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -2,7 +2,6 @@ var RealMath = artifacts.require("./RealMath.sol");
 var RNG = artifacts.require("./RNG.sol");
 var MacroversePrototype = artifacts.require("./MacroversePrototype.sol")
 var MacroverseStarRegistry = artifacts.require("./MacroverseStarRegistry.sol")
-var SingleStarPrototype = artifacts.require("./SingleStarPrototype.sol")
 
 var MRVToken = artifacts.require("./MRVToken.sol")
 var MinimumBalanceAccessControl = artifacts.require("./MinimumBalanceAccessControl.sol")
@@ -14,20 +13,18 @@ module.exports = async function(deployer, network, accounts) {
   deployer.deploy(RNG)
   deployer.link(RNG, MacroversePrototype)
   deployer.link(RealMath, MacroversePrototype)
-  deployer.link(RNG, SingleStarPrototype)
-  deployer.link(RealMath, SingleStarPrototype)
 
   deployer.deploy(UnrestrictedAccessControl)
   
   // Deploy the token
   await deployer.deploy(MRVToken, accounts[0]).then(function() {
   
-    // Deploy a minimum balance access control strategy
+    // Deploy a minimum balance access control strategy, with a 100 MRV minimum balance requirement.
     return deployer.deploy(MinimumBalanceAccessControl, MRVToken.address, web3.toWei(100, "ether")).then(async function() {
-      // Deploy the actual MG prototypes and point them initially at that access control contract.
+      // Deploy the actual MG prototype and point it initially at that access control contract.
       await deployer.deploy(MacroversePrototype, "prototypeseed12", MinimumBalanceAccessControl.address)
-      await deployer.deploy(SingleStarPrototype, "prototypeseed12", MinimumBalanceAccessControl.address)
-      
+      // Deploy the star ownership registry, with a 1000 MRV minimum ownership deposit.
+      await deployer.deploy(MacroverseStarRegistry, MRVToken.address, web3.toWei(1000, "ether"))
     })
   })
   
