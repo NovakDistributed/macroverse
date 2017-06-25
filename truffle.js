2bd444ca5c5130adfa0de9e27444c1ca5dbac8f7
Macroverse <macroverse@protonmail.com>
2017-06-24 22:40:47 -0700

    Truffle utterly failed to deploy reasonably and now I have to clean up its failed migration

--- truffle.js
+++ truffle.js
@@ -9,7 +9,9 @@ module.exports = {
       network_id: 1,
       host: "localhost",
       port: 8546,   // Different than the default
-      from: "0x368651F6c2b3a7174ac30A5A062b65F2342Fb6F1"
+      from: "0x368651F6c2b3a7174ac30A5A062b65F2342Fb6F1",
+      gas: 4700000, // Knock down because it has to be les than block gas limit
+      gasPrice: 31000000000 // Defaults to 100 gwei = 100 shannon = 100 billion, which is extremely high.
     }
   }
 };
