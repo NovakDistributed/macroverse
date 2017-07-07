f500e44321c049fd7922d4bcc650c5052c256306
Macroverse <macroverse@protonmail.com>
2017-07-06 23:10:14 -0700

    Set gas price to 4 billion

--- truffle.js
+++ truffle.js
@@ -11,7 +11,7 @@ module.exports = {
       port: 8546,   // Different than the default
       from: "0x368651F6c2b3a7174ac30A5A062b65F2342Fb6F1",
       gas: 4700000, // Knock down because it has to be les than block gas limit
-      gasPrice: 49000000000 // Defaults to 100 gwei = 100 shannon = 100 billion, which is extremely high.
+      gasPrice: 4000000000 // Defaults to 100 gwei = 100 shannon = 100 billion, which is extremely high.
     }
   }
 };
