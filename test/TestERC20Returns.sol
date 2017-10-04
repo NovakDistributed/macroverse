b128361c6a1fb43e753ce49576fd384ea8eae899
Macroverse <macroverse@protonmail.com>
2017-10-03 20:48:14 -0700

    Use a much lighter OpenZeppelin dependency

--- test/TestERC20Returns.sol
+++ test/TestERC20Returns.sol
@@ -2,13 +2,23 @@ pragma solidity ^0.4.11;
 
 import "truffle/Assert.sol";
 import "truffle/DeployedAddresses.sol";
-import "zeppelin-solidity/contracts/token/ERC20.sol";
 
 // The real ERC20 standard has approve, transfer, and transferFrom returning bools, but the
-// OpenZeppelin implementation just has them throw/not throw.
+// OpenZeppelin implementation that was used for the MRV token just has them throw/not throw.
 // It's recommented for an ERC20 token to throw when it would return false, so we just have
 // to make sure that when it doesn't throw that's the same to callers expecting the
 // standard-compliant ABI as having returned true.
+
+contract FakeERC20 {
+    function totalSupply() constant returns (uint totalSupply);
+    function balanceOf(address _owner) constant returns (uint balance);
+    function transfer(address _to, uint _value);
+    function transferFrom(address _from, address _to, uint _value);
+    function approve(address _spender, uint _value);
+    function allowance(address _owner, address _spender) constant returns (uint remaining);
+    event Transfer(address indexed _from, address indexed _to, uint _value);
+    event Approval(address indexed _owner, address indexed _spender, uint _value);
+}
 
 contract RealERC20 {
     function totalSupply() constant returns (uint totalSupply);
@@ -21,7 +31,10 @@ contract RealERC20 {
     event Approval(address indexed _owner, address indexed _spender, uint _value);
 }
 
-contract SuccessCoin is ERC20 {
+contract SuccessCoin is FakeERC20 {
+    function totalSupply() constant returns (uint totalSupply) {
+        return 99999;
+    }
     function transfer(address to, uint value) {
         // Say it worked by not throwing
     }
