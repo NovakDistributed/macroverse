f7cef5bac25634dc65079b160b4a932137921823
Macroverse <macroverse@protonmail.com>
2017-10-10 21:52:59 -0700

    Get a start on the MacroverseSystemGenerator.

---
+++ contracts/MacroverseSystemGenerator.sol
@@ -0,0 +1,82 @@
+pragma solidity ^0.4.11;
+
+import "./RNG.sol";
+import "./RealMath.sol";
+
+import "./AccessControl.sol";
+import "./ControlledAccess.sol";
+
+import "./MacroverseStarGenerator.sol";
+
+/**
+ * Represents a prorotype Macroverse Generator for a galaxy.
+ *
+ * Permission to call methods on this contract is regulated by a configurable
+ * AccessControl contract. One such set of terms might be to require that the
+ * account initiating a transaction have a certain minimum MRV token balance.
+ *
+ * The owner of this contract reserves the right to supersede it with a new
+ * version, and to modify the terms for accessing this contract, at any time,
+ * for any reason, and without notice. This includes the right to indefinitely
+ * or permanently suspend or terminate access to this contract for any person,
+ * account, or other contract, or for all persons, accounts, or other
+ * contracts. The owner also reserves the right to not do any of the above.
+ */
+contract MacroverseSystemGenerator is ControlledAccess {
+    // TODO: RNG doesn't get linked against because we can't pass the struct to the library...
+    using RNG for *;
+    using RealMath for *;
+    // No SafeMath or it might confuse RealMath
+
+    /**
+     * Deploy a new copy of the MacroverseSystemGenerator.
+     */
+    function MacroverseSystemGenerator(address accessControlAddress) ControlledAccess(AccessControl(accessControlAddress)) {
+        
+    }
+    
+    /**
+     * If the object has any planets at all, get theœplanet count. Will return
+     * nonzero numbers always, so make sure to check getObjectHasPlanets in the
+     * Star Generator.
+     */
+    function getObjectPlanetCount(bytes32 seed, MacroverseStarGenerator.ObjectClass objectClass,
+        MacroverseStarGenerator.SpectralType spectralType) constant onlyControlledAccess returns (uint16) {
+        
+        var node = RNG.RandNode(seed).derive("planetcount");
+        
+        
+        int16 limit;
+
+        if (objectClass == MacroverseStarGenerator.ObjectClass.MainSequence) {
+            if (spectralType == MacroverseStarGenerator.SpectralType.TypeO ||
+                spectralType == MacroverseStarGenerator.SpectralType.TypeB) {
+                
+                limit = 5;
+            } else if (spectralType == MacroverseStarGenerator.SpectralType.TypeA) {
+                limit = 7;
+            } else if (spectralType == MacroverseStarGenerator.SpectralType.TypeF ||
+                spectralType == MacroverseStarGenerator.SpectralType.TypeG ||
+                spectralType == MacroverseStarGenerator.SpectralType.TypeK) {
+                
+                limit = 12;
+            } else if (spectralType == MacroverseStarGenerator.SpectralType.TypeM) {
+                limit = 14;
+            }
+        } else if (objectClass == MacroverseStarGenerator.ObjectClass.Giant) {
+            limit = 2;
+        } else if (objectClass == MacroverseStarGenerator.ObjectClass.Supergiant) {
+            limit = 2;
+        } else {
+           // Black hole, neutron star, or white dwarf
+           limit = 2;
+        }
+        
+        uint16 roll = uint16(node.getIntBetween(1, limit));
+        
+        return roll;
+    }
+    
+
+}
+ 
