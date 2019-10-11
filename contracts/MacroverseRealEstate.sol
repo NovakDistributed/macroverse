25dc4f9cf9b774a37a1251c0308a76e75ff8ed56
Macroverse <macroverse@protonmail.com>
2019-10-10 22:02:46 -0700

    Split the MacroverseUniversalRegistry so the actual token is a separate MacroverseRealEstate contract

---
+++ contracts/MacroverseRealEstate.sol
@@ -0,0 +1,50 @@
+pragma solidity ^0.4.24;
+
+import "openzeppelin-solidity/contracts/token/ERC721/ERC721Full.sol";
+
+import "openzeppelin-solidity/contracts/ownership/Ownable.sol";
+
+/**
+ * The MacroverseRealEstate contract keeps track of who currently owns virtual
+ * real estate in the Macroverse world, at all scales. It supersedes the
+ * MacroverseStarRegistry. Registration and Macroverse-specific manipulation of
+ * tokens is accomplished through the MacroverseUniversalRegistry, which owns
+ * this contract.
+ *
+ * The split between this contract and the MacroverseUniversalRegistry exists
+ * to keep contract size under the limit. 
+ */
+contract MacroverseRealEstate is ERC721Full, Ownable {
+
+    
+    /**
+     * Deploy the backend, taking mint, burn, and set-user commands from the deployer.
+     */
+    constructor() public ERC721Full("Macroverse Real Estate", "MRE") {
+    }
+
+    /**
+     * Mint tokens at the direction of the owning contract.
+     */
+    function mint(address to, uint256 tokenId) external onlyOwner {
+        _mint(to, tokenId);
+    }
+
+    /**
+     * Burn tokens at the direction of the owning contract.
+     */
+    function burn(address from, uint256 tokenId) external onlyOwner {
+        _burn(from, tokenId);
+    }
+
+    /**
+     * Publically expose a token existence test. Returns true if the given
+     * token is owned by someone, and false otherwise. Note that tokens sent to
+     * 0x0 but not burned may still exist.
+     */
+    function exists(uint256 tokenId) external view returns (bool) {
+        return _exists(tokenId);
+    }
+
+    
+}
--- migrations/12_deploy_universal_registry.js
+++ migrations/12_deploy_universal_registry.js
@@ -3,6 +3,7 @@ var TestnetMRVToken = artifacts.require("./TestnetMRVToken.sol")
 var MacroverseNFTUtils = artifacts.require("./MacroverseNFTUtils.sol")
 var MacroverseExistenceChecker = artifacts.require("./MacroverseExistenceChecker.sol")
 var MacroverseUniversalRegistry = artifacts.require("./MacroverseUniversalRegistry.sol")
+var MacroverseRealEstate = artifacts.require("./MacroverseRealEstate.sol")
 
 module.exports = async function(deployer, network, accounts) {
 
@@ -14,7 +15,17 @@ module.exports = async function(deployer, network, accounts) {
 
   // Deploy the registry, using the existence checker and the existing token
   // and a starting min star deposit, with a 1 day commitment maturation time.
-  return deployer.deploy(MacroverseUniversalRegistry, MacroverseExistenceChecker.address,
-    token_contract.address, web3.toWei(1000, "ether"), 60 * 60 * 24)
+  return deployer.deploy(MacroverseRealEstate).then(function() {
+    return deployer.deploy(MacroverseUniversalRegistry, MacroverseRealEstate.address,
+      MacroverseExistenceChecker.address, token_contract.address, 
+      web3.toWei(1000, "ether"), 60 * 60 * 24)
+  }).then(function() {
+    return MacroverseRealEstate.deployed() 
+  }).then(function(backend) {
+    // Give the backend to the frontend
+    return backend.transferOwnership(MacroverseUniversalRegistry.address)
+  })
+  
+  
   
 };
