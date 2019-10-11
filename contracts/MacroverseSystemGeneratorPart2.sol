9a3b634f588c930a40c2a8b05ea0a06aad5a3c29
Macroverse <macroverse@protonmail.com>
2019-10-10 20:44:21 -0700

    Split up the system generator

---
+++ contracts/MacroverseSystemGeneratorPart2.sol
@@ -0,0 +1,210 @@
+pragma solidity ^0.4.24;
+
+import "./RNG.sol";
+import "./RealMath.sol";
+
+import "./AccessControl.sol";
+import "./ControlledAccess.sol";
+
+import "./Macroverse.sol";
+
+
+/**
+ * Contains a portion of the MacroverseStstemGenerator implementation code.
+ * The contract is split up due to contract size limitations.
+ * We can't do access control here sadly.
+ */
+library MacroverseSystemGeneratorPart2 {
+    using RNG for *;
+    using RealMath for *;
+    // No SafeMath or it might confuse RealMath
+
+    /**
+     * It is useful to have Pi around.
+     * We can't pull it in from the library.
+     */
+    int128 constant REAL_PI = 3454217652358;
+
+    /**
+     * Also perpare pi/2
+     */
+    int128 constant REAL_HALF_PI = REAL_PI >> 1;
+
+    /**
+     * How many fractional bits are there?
+     */
+    int256 constant REAL_FBITS = 40;
+    
+    /**
+     * What's the first non-fractional bit
+     */
+    int128 constant REAL_ONE = int128(1) << REAL_FBITS;
+    
+    /**
+     * What's the last fractional bit?
+     */
+    int128 constant REAL_HALF = REAL_ONE >> 1;
+    
+    /**
+     * What's two? Two is pretty useful.
+     */
+    int128 constant REAL_TWO = REAL_ONE << 1;
+
+    /**
+     * And zero
+     */
+    int128 constant REAL_ZERO = 0;
+    
+    /**
+     * Convert from periapsis and apoapsis to semimajor axis and eccentricity.
+     */
+    function convertOrbitShape(int128 realPeriapsis, int128 realApoapsis) public pure returns (int128 realSemimajor, int128 realEccentricity) {
+        // Semimajor axis is average of apoapsis and periapsis
+        realSemimajor = RealMath.div(realApoapsis + realPeriapsis, RealMath.toReal(2));
+        
+        // Eccentricity is ratio of difference and sum
+        realEccentricity = RealMath.div(realApoapsis - realPeriapsis, realApoapsis + realPeriapsis);
+    }
+    
+    // Define the orbital plane
+    
+    /**
+     * Get the longitude of the ascending node for a planet or moon. For
+     * planets, this is the angle from system +X to ascending node. For
+     * moons, we use system +X transformed into the planet's equatorial plane
+     * by the equatorial plane/rotation axis angles.
+     */ 
+    function getWorldLan(bytes32 seed) public pure returns (int128) {
+        RNG.RandNode memory node = RNG.RandNode(seed).derive("LAN");
+        // Angles should be uniform from 0 to 2 PI
+        return node.getRealBetween(RealMath.toReal(0), RealMath.mul(RealMath.toReal(2), REAL_PI));
+    }
+    
+    /**
+     * Get the inclination (angle from system XZ plane to orbital plane at the ascending node) for a planet.
+     * For a moon, this is done in the moon generator instead.
+     * Inclination is always positive. If it were negative, the ascending node would really be the descending node.
+     * Result is a real in radians.
+     */ 
+    function getPlanetInclination(bytes32 seed, Macroverse.WorldClass class) public pure returns (int128) {
+        RNG.RandNode memory node = RNG.RandNode(seed).derive("inclination");
+    
+        // Define minimum and maximum inclinations in milliradians
+        // 175 milliradians = ~ 10 degrees
+        int88 minimum;
+        int88 maximum;
+        if (class == Macroverse.WorldClass.Lunar || class == Macroverse.WorldClass.Europan) {
+            minimum = 0;
+            maximum = 175;
+        } else if (class == Macroverse.WorldClass.Terrestrial || class == Macroverse.WorldClass.Panthalassic) {
+            minimum = 0;
+            maximum = 87;
+        } else if (class == Macroverse.WorldClass.Neptunian) {
+            minimum = 0;
+            maximum = 35;
+        } else if (class == Macroverse.WorldClass.Jovian) {
+            minimum = 0;
+            maximum = 52;
+        } else if (class == Macroverse.WorldClass.AsteroidBelt) {
+            minimum = 0;
+            maximum = 262;
+        } else {
+            // Not real!
+            revert();
+        }
+        
+        // Decide if we should be retrograde (PI-ish inclination)
+        int128 real_retrograde_offset = 0;
+        if (node.derive("retrograde").d(1, 100, 0) < 3) {
+            // This planet ought to move retrograde
+            real_retrograde_offset = REAL_PI;
+        }
+
+        return real_retrograde_offset + RealMath.div(node.getRealBetween(RealMath.toReal(minimum), RealMath.toReal(maximum)), RealMath.toReal(1000));    
+    }
+    
+    // Define the orbit's embedding in the plane (and in time)
+    
+    /**
+     * Get the argument of periapsis (angle from ascending node to periapsis position, in the orbital plane) for a planet or moon.
+     */
+    function getWorldAop(bytes32 seed) public pure returns (int128) {
+        RNG.RandNode memory node = RNG.RandNode(seed).derive("AOP");
+        // Angles should be uniform from 0 to 2 PI.
+        // We already made sure planets/moons wouldn't get too close together when laying out the orbits.
+        return node.getRealBetween(RealMath.toReal(0), RealMath.mul(RealMath.toReal(2), REAL_PI));
+    }
+    
+    /**
+     * Get the mean anomaly (which sweeps from 0 at periapsis to 2 pi at the next periapsis) at epoch (time 0) for a planet or moon.
+     */
+    function getWorldMeanAnomalyAtEpoch(bytes32 seed) public pure returns (int128) {
+        RNG.RandNode memory node = RNG.RandNode(seed).derive("MAE");
+        // Angles should be uniform from 0 to 2 PI.
+        return node.getRealBetween(RealMath.toReal(0), RealMath.mul(RealMath.toReal(2), REAL_PI));
+    }
+
+    /**
+     * Determine if the world is tidally locked, given its seed and its number
+     * out from the parent, starting with 0.
+     * Overrides getWorldZXAxisAngles and getWorldSpinRate. 
+     * Not used for asteroid belts or rings.
+     */
+    function isTidallyLocked(bytes32 seed, uint16 worldNumber) public pure returns (bool) {
+        // Tidal lock should be common near the parent and less common further out.
+        return RNG.RandNode(seed).derive("tidal_lock").getReal() < RealMath.fraction(1, int88(worldNumber + 1));
+    }
+
+    /**
+     * Get the Y and X axis angles for a world, in radians.
+     * The world's rotation axis starts straight up in its orbital plane.
+     * Then the planet is rotated in Y, around the axis by the Y angle.
+     * Then it is rotated forward (what would be toward the pureer) in the
+     * world's transformed X by the X axis angle.
+     * Both angles are in radians.
+     * The X angle is never negative, because the Y angle would just be the opposite direction.
+     * It is also never greater than Pi, because otherwise we would just measure around the other way.
+     * Not used for asteroid belts or rings.
+     * For a tidally locked world, ignore these values and use 0 for both angles.
+     */
+    function getWorldYXAxisAngles(bytes32 seed) public pure returns (int128 realYRadians, int128 realXRadians) {
+       
+        // The Y angle should be uniform over all angles.
+        realYRadians = RNG.RandNode(seed).derive("axisy").getRealBetween(-REAL_PI, REAL_PI);
+
+        // The X angle will be mostly small positive or negative, with some sideways and some near Pi/2 (meaning retrograde rotation)
+        int16 tilt_die = RNG.RandNode(seed).derive("tilt").d(1, 6, 0);
+        
+        // Start with low tilt, right side up
+        // Earth is like 0.38 radians overall
+        int128 real_tilt_limit = REAL_HALF;
+        if (tilt_die >= 5) {
+            // Be high tilt
+            real_tilt_limit = REAL_HALF_PI;
+        }
+    
+        RNG.RandNode memory x_node = RNG.RandNode(seed).derive("axisx");
+        realXRadians = x_node.getRealBetween(0, real_tilt_limit);
+
+        if (tilt_die == 4 || tilt_die == 5) {
+            // Flip so the tilt we have is relative to upside-down
+            realXRadians = REAL_PI - realXRadians;
+        }
+
+        // So we should have 1/2 low tilt prograde, 1/6 low tilt retrograde, 1/6 high tilt retrograde, and 1/6 high tilt prograde
+    }
+
+    /**
+     * Get the spin rate of the world in radians per Julian year around its axis.
+     * For a tidally locked world, ignore this value and use the mean angular
+     * motion computed by the OrbitalMechanics contract, given the orbit
+     * details.
+     * Not used for asteroid belts or rings.
+     */
+    function getWorldSpinRate(bytes32 seed) public pure returns (int128) {
+        // Earth is something like 2k radians per Julian year.
+        return RNG.RandNode(seed).derive("spin").getRealBetween(REAL_ZERO, RealMath.toReal(8000)); 
+    }
+
+}
+ 
--- migrations/6_deploy_system_generator.js
+++ migrations/6_deploy_system_generator.js
@@ -1,16 +1,28 @@
 var RealMath = artifacts.require("./RealMath.sol");
 var RNG = artifacts.require("./RNG.sol");
+var Macroverse = artifacts.require("./Macroverse.sol")
+var MacroverseSystemGeneratorPart1 = artifacts.require("./MacroverseSystemGeneratorPart1.sol")
+var MacroverseSystemGeneratorPart2 = artifacts.require("./MacroverseSystemGeneratorPart2.sol")
 var MacroverseSystemGenerator = artifacts.require("./MacroverseSystemGenerator.sol")
 var MinimumBalanceAccessControl = artifacts.require("./MinimumBalanceAccessControl.sol")
 
-module.exports = async function(deployer, network, accounts) {
-  
-  // Link
-  deployer.link(RNG, MacroverseSystemGenerator)
-  deployer.link(RealMath, MacroverseSystemGenerator)
+module.exports = function(deployer, network, accounts) {
+  return deployer.deploy(Macroverse).then(function() {
+    // Link
+    deployer.link(RNG, MacroverseSystemGeneratorPart1)
+    deployer.link(RealMath, MacroverseSystemGeneratorPart1)
 
-  // And deploy the system generator, using the existing access control.
-  await deployer.deploy(MacroverseSystemGenerator, MinimumBalanceAccessControl.address)
-      
-  
+    deployer.link(RNG, MacroverseSystemGeneratorPart2)
+    deployer.link(RealMath, MacroverseSystemGeneratorPart2)
+
+    return deployer.deploy(MacroverseSystemGeneratorPart1)
+  }).then(function() {
+    return deployer.deploy(MacroverseSystemGeneratorPart2)
+  }).then(function() {
+    deployer.link(MacroverseSystemGeneratorPart1, MacroverseSystemGenerator)
+    deployer.link(MacroverseSystemGeneratorPart2, MacroverseSystemGenerator)
+    
+    // And deploy the system generator, using the existing access control.
+    return deployer.deploy(MacroverseSystemGenerator, MinimumBalanceAccessControl.address)
+  })
 };
